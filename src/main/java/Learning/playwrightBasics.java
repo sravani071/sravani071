6f120d00b2715c6f9e4ae73f5bfec07f3ef1c790
Sravani Koripalli <koripalli.sravani@servicenow.com>
2023-08-18 17:24:39 +0530

    modified file

--- src/main/java/Learning/playwrightBasics.java
+++ src/main/java/Learning/playwrightBasics.java
@@ -3,7 +3,11 @@ package Learning;
 public class playwrightBasics {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//System.out.println("test");
+		int a =10;
+		int b = 20;
+		int c = a+b;
+		System.out.println(c);
 
 	}
 
