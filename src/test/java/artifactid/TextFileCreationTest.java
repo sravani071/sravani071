6999ca00f2ec77e8929fe4554602aa674263b621
Sravani Koripalli <koripalli.sravani@servicenow.com>
2023-09-25 16:56:27 +0530

    pushing changes wuth parameter in a class

--- src/test/java/artifactid/TextFileCreationTest.java
+++ src/test/java/artifactid/TextFileCreationTest.java
@@ -10,16 +10,18 @@ import org.junit.Test;
 
 
 public class TextFileCreationTest {
+	
     @Test
-	public void FileCreation() throws IOException{
+	public void FileCreation(String input) throws IOException{
 	    //try {
 	     //String ts = new Date().toString();
+    	input = System.getProperty("parm");
 		String ts = new SimpleDateFormat("ddMMyyyy_hhmmss").format(new Date());  
 	      File myObj = new File("src/test/resources/FileCreation/"+ts+".txt");
 	      if (myObj.createNewFile()) {
 	        System.out.println("File created: " + myObj.getName());
 	        FileWriter fWriter = new FileWriter("src/test/resources/FileCreation/"+myObj.getName());
-	            fWriter.write("Hi Sravani");
+	            fWriter.write(input);
 	            fWriter.close();
 	      } else {
 	        System.out.println("File already exists.");
