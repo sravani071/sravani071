48a11fdb4158f6e3894e1c2df17fcfbf89a8f367
Sravani Koripalli <koripalli.sravani@servicenow.com>
2023-09-25 19:55:44 +0530

    Object change

--- src/test/java/artifactid/TextFileCreationTest.java
+++ src/test/java/artifactid/TextFileCreationTest.java
@@ -15,13 +15,13 @@ public class TextFileCreationTest {
 	public void FileCreation() throws IOException{
 	    //try {
 	     //String ts = new Date().toString();
-    	String input = System.getProperty("parm");
+    	Object input = System.getProperty("parm");
 		String ts = new SimpleDateFormat("ddMMyyyy_hhmmss").format(new Date());  
 	      File myObj = new File("src/test/resources/FileCreation/"+ts+".txt");
 	      if (myObj.createNewFile()) {
 	        System.out.println("File created: " + myObj.getName());
 	        FileWriter fWriter = new FileWriter("src/test/resources/FileCreation/"+myObj.getName());
-	            fWriter.write(input);
+	            fWriter.write((String)input);
 	            fWriter.close();
 	      } else {
 	        System.out.println("File already exists.");
